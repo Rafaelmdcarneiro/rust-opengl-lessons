8eff385f7041c63bea823f133c9a1959a8936550
Rafael Carneiro <rafaelmdcarneiro@gmail.com>
2024-05-26 10:57:01 -0300

    Add files via upload

---
+++ lesson-07/src/resources.rs
@@ -0,0 +1,62 @@
+use std::ffi;
+use std::fs;
+use std::io::{self, Read};
+use std::path::{Path, PathBuf};
+
+#[derive(Debug)]
+pub enum Error {
+    Io(io::Error),
+    FileContainsNil,
+    FailedToGetExePath,
+}
+
+impl From<io::Error> for Error {
+    fn from(other: io::Error) -> Self {
+        Error::Io(other)
+    }
+}
+
+pub struct Resources {
+    root_path: PathBuf,
+}
+
+impl Resources {
+    pub fn from_relative_exe_path(rel_path: &Path) -> Result<Resources, Error> {
+        let exe_file_name = ::std::env::current_exe().map_err(|_| Error::FailedToGetExePath)?;
+
+        let exe_path = exe_file_name.parent().ok_or(Error::FailedToGetExePath)?;
+
+        Ok(Resources {
+            root_path: exe_path.join(rel_path),
+        })
+    }
+
+    pub fn from_exe_path() -> Result<Resources, Error> {
+        Resources::from_relative_exe_path(Path::new(""))
+    }
+
+    pub fn load_cstring(&self, resource_name: &str) -> Result<ffi::CString, Error> {
+        let mut file = fs::File::open(resource_name_to_path(&self.root_path, resource_name))?;
+
+        // allocate buffer of the same size as file
+        let mut buffer: Vec<u8> = Vec::with_capacity(file.metadata()?.len() as usize + 1);
+        file.read_to_end(&mut buffer)?;
+
+        // check for nul byte
+        if buffer.iter().find(|i| **i == 0).is_some() {
+            return Err(Error::FileContainsNil);
+        }
+
+        Ok(unsafe { ffi::CString::from_vec_unchecked(buffer) })
+    }
+}
+
+fn resource_name_to_path(root_dir: &Path, location: &str) -> PathBuf {
+    let mut path: PathBuf = root_dir.into();
+
+    for part in location.split("/") {
+        path = path.join(part);
+    }
+
+    path
+}
